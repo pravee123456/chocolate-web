397ac6469b98ca24ca2b90efafc5c72f5aeccd6a
pravee123456 <swethagnanasundar@gmail.com>
2024-05-31 14:12:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+burger=document.querySelector('.burger')
+navbarItems=document.querySelector('.navbar')
+nav=document.querySelector('.items')
+burger.addEventListener('click',()=>{
+navbarItems.classList.toggle('h-class')
+nav.classList.toggle('v-class')
+})
